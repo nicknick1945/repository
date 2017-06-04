94978bad05d15ffe512e9127f45203ac4926a303
nick <n.matukhin@simplex-software.ru>
2017-06-05 00:51:42 +0300

    Добавлены методы в абстрактный обучающий класс , добавлен тестовый метод для тестиирования правильности работы сверточных слоев

--- src/ru/mycompany/main/neuronswide/neurons_wide_learner/AbstractImpl/NeuronsNetworkLearnerAbstractImpl.java
+++ src/ru/mycompany/main/neuronswide/neurons_wide_learner/AbstractImpl/NeuronsNetworkLearnerAbstractImpl.java
@@ -1,7 +1,7 @@
 package ru.mycompany.main.neuronswide.neurons_wide_learner.AbstractImpl;
 
-import ru.mycompany.main.neuronswide.neuron_wide.interfaces.LearningSample;
-import ru.mycompany.main.neuronswide.neuron_wide.interfaces.NeuronNetwork;
+import ru.mycompany.main.neuronswide.neuron_wide.interfaces.*;
+import ru.mycompany.main.neuronswide.neurons_wide_learner.exceptions.NotMatchCountNeuronsOutAndSamplOut;
 import ru.mycompany.main.neuronswide.neurons_wide_learner.interfaces.NeuronsNetworkLearner;
 
 /**
@@ -15,6 +15,8 @@ public class NeuronsNetworkLearnerAbstractImpl implements NeuronsNetworkLearner{
     private double[][] linksErrors;
     /**Собственно нейросеть*/
     private NeuronNetwork neuronNetwork;
+    //Коэффициент скорости обученя
+    private double learningSpeed = 0.001;
 
     /**конструктор в качестве аргумента полкчаем нейросеть*/
     public NeuronsNetworkLearnerAbstractImpl(NeuronNetwork neuronNetwork) {
@@ -26,15 +28,60 @@ public class NeuronsNetworkLearnerAbstractImpl implements NeuronsNetworkLearner{
 
     }
 
-    private void upError(){
+    @Override
+    public void doLearn(LearningSample[] learningSamples, int countEpoch) {
+        for(int i = 0 ; i < countEpoch ; i ++ ){
+            learnEpoch(learningSamples);
+        }
+    }
+
+    /**Этпоха обучения для обучения без лимита эпох,
+     * обучение ведется до тех пор пока разнца между требуемым вызодом
+     * и реальных не станем меньше переменной delta
+     * @patam delta ошибка при одстижении которой прекращается обучение
+     *
+     * */
+    protected void learnLimitedEpoch(LearningSample[] learningSamples,double delta){
 
     }
 
-    private void corectingWidth(){
+    /**метод поднятия ошибки , зополняет массив ошибок каждого уровня*/
+    protected void upError(){
 
     }
 
-    private void learnEpohe(){
+    /**метод корректировки весов*/
+    protected void correctingWidth(){
 
     }
+
+    /**Эпоха обучения для метода обучения с лимитированным колличеством эпох*/
+    protected double[] learnEpoch(LearningSample[] learningSamples){
+        return null; // todo переписать когда напиешь функцию
+    }
+
+    protected double getLearningSpeed(){
+        return learningSpeed;
+    }
+
+    protected void setLearningSpeed(double learnSpeed){
+        this.learningSpeed = learnSpeed;
+    }
+
+    /**Функция рассчета ошики уровня */
+    protected double[] calculateLayerErrors(NeuronsLayer neuronsLayer,double[] targerResult) throws Exception {
+
+        Neuron[] neurons = neuronsLayer.getNeurons();
+        double[] errors = new double[neurons.length];
+
+        if (neurons.length != targerResult.length) {
+            throw  new NotMatchCountNeuronsOutAndSamplOut();
+        }
+
+        for(int i = 0 ; i < neurons.length ; i ++){
+            errors[i] = targerResult[i] - (double)neurons[i].getState();
+        }
+        return errors;
+    }
+
 }
